4cfaeb408ad4500a86b24baf03aa687d4ef868c8
Eugene <edukalis@gmail.com>
2022-10-24 19:21:59 +0300

    completed swiper js slider. moving to next slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
-    $('.slider__hero').slick({
-        arrows: false,
-        dots : true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 1000,
-        infinite: true,
-    });
+$(document).ready(function () {
+  $('.slider__hero').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    infinite: true,
   });
+});
 
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 5,
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 5,
   spaceBetween: 10,
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
