08cdcc4676746016480de5375d4c1bddcc8546b7
Eugene <edukalis@gmail.com>
2022-10-24 17:17:34 +0300

    created structure

---
+++ source/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__hero').slick({
+        arrows: false,
+        dots : true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+    });
+  });
