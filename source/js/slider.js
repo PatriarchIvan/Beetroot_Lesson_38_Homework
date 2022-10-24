0a27e4ea3fb78e8724296a5f8e684a2760668bfa
Eugene <edukalis@gmail.com>
2022-10-24 18:01:43 +0300

    fully completed first slider using slick

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,5 +5,17 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 1000,
+        infinite: true,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider__shop').slick({
+        arrows: false,
+        dots : false,
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
     });
   });
