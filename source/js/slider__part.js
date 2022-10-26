0b30d8c8ec166e7a1396990224ab2b09a7474344
Eugene <edukalis@gmail.com>
2022-10-26 11:37:59 +0300

    completed cascade, moving to adapt

--- source/js/slider__part.js
+++ source/js/slider__part.js
@@ -1,10 +1,11 @@
+const menu = ['', '', ''];
 const swiperPart = new Swiper('.partners.swiper', {
     slidesPerView: 9,
     spaceBetween: 20,
     loop: true,
     autoplay: {
         delay: 1500,
-        disableOnInteraction: true, 
+        disableOnInteraction: true,
         pauseOnMouseEnter: true,
         waitForTransition: true,
     },
@@ -16,7 +17,35 @@ const swiperPart = new Swiper('.partners.swiper', {
         nextEl: '.swiper-button-next-unique-part',
         prevEl: '.swiper-button-prev-unique-part',
     },
-    // pagination: {
-    //     el: '.swiper-pagination',
-    //   },
+
+    pagination: {
+        el: '.swiper-pagination',
+        enabled: false,
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            pagination: {
+                enabled: true,
+            }
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+        // when window width is >= 920px
+        920: {
+            slidesPerView: 9,
+            spaceBetween: 40,
+        },
+    },
+
 });
